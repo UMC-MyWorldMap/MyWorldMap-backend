57c4dc5f1959799605891c1e2a66310ecd6c9026
yyypearl <hor629@naver.com>
2025-01-11 00:44:21 +0900

    [feature] 도시, 국가명 혼합 검색 기능 추가 #112

--- src/main/java/umc/TripPiece/repository/CountryRepository.java
+++ src/main/java/umc/TripPiece/repository/CountryRepository.java
@@ -1,6 +1,9 @@
 package umc.TripPiece.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import umc.TripPiece.domain.City;
 import umc.TripPiece.domain.Country;
 
 import java.math.BigInteger;
@@ -8,4 +11,7 @@ import java.util.List;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
     List<Country> findByNameIgnoreCase(String name);
+
+    @Query("SELECT c FROM Country c WHERE :query LIKE CONCAT('%', c.name, '%')")
+    List<Country> findCountriesInSearch(@Param("query") String query);
 }
