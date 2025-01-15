f7f1a0e03a4865a5d1e2741d7d4f41ae0e0f437c
StoneCAU <seokwoo1999@naver.com>
2025-01-15 23:52:45 +0900

    [feature] Token 관련 예외 처리 구현

--- src/main/java/umc/TripPiece/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/umc/TripPiece/apiPayload/code/status/ErrorStatus.java
@@ -48,8 +48,12 @@ public enum ErrorStatus implements BaseErrorCode {
 
     INVALID_PROFILE_IMAGE(HttpStatus.BAD_REQUEST, "PROFILE400", "잘못된 프로필 이미지 형식입니다."),
     PROFILE_UPDATE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "PROFILE500", "프로필 업데이트에 실패했습니다."),
-  
+
+    // 토큰 관련 오류
     INVALID_TOKEN(HttpStatus.BAD_REQUEST, "TOKEN401", "유효하지 않은 토큰입니다."),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "TOKEN401", "만료된 토큰입니다."),
+    UNSUPPORTED_TOKEN(HttpStatus.BAD_REQUEST, "TOKEN402", "지원하지 않는 형식의 토큰입니다."),
+    NOT_FOUND_TOKEN(HttpStatus.NOT_FOUND, "TOKEN404", "토큰의 클레임이 비어있습니다."),
 
     GENERAL_BAD_REQUEST(HttpStatus.BAD_REQUEST, "SOCIAL400", "일반 회원가입에 대한 요청입니다."),
     PLATFORM_BAD_REQUEST(HttpStatus.BAD_REQUEST, "SOCIAL400", "유효하지 않은 소셜 플랫폼입니다. (KAKAO 또는 APPLE만 허용)"),
@@ -80,6 +84,7 @@ public enum ErrorStatus implements BaseErrorCode {
 
     // 지도 관련 오류
     INVALID_CITY_COUNTRY_RELATION(HttpStatus.BAD_REQUEST, "MAP400", "유효하지 않은 도시와 국가 관계입니다.");
+
 
     private final HttpStatus httpStatus;
     private final String code;
--- src/main/java/umc/TripPiece/security/JwtExceptionFilter.java
+++ src/main/java/umc/TripPiece/security/JwtExceptionFilter.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import umc.TripPiece.apiPayload.code.status.ErrorStatus;
+import umc.TripPiece.apiPayload.exception.GeneralException;
 
 @Component
 @RequiredArgsConstructor
@@ -18,11 +20,27 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws IOException, ServletException {
         try {
             filterChain.doFilter(request, response);
-        } catch (RuntimeException e) {
-            System.out.println("-------------------------------jwtExfilter");
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write("Unauthorized: " + e.getMessage());
+        } catch (GeneralException e) {
+            String message = e.getMessage();
+
+            if (ErrorStatus.INVALID_TOKEN.getMessage().equals(message)) {
+                setResponse(response, ErrorStatus.INVALID_TOKEN);
+            }
+            else if (ErrorStatus.TOKEN_EXPIRED.getMessage().equals(message)) {
+                setResponse(response, ErrorStatus.TOKEN_EXPIRED);
+            }
+            else if (ErrorStatus.UNSUPPORTED_TOKEN.getMessage().equals(message)) {
+                setResponse(response, ErrorStatus.UNSUPPORTED_TOKEN);
+            }
+            else if (ErrorStatus.NOT_FOUND_TOKEN.getMessage().equals(message)) {
+                setResponse(response, ErrorStatus.NOT_FOUND_TOKEN);
+            }
         }
     }
 
+    private void setResponse(HttpServletResponse response, ErrorStatus errorStatus) throws RuntimeException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(errorStatus.getHttpStatus().value());
+        response.getWriter().print(errorStatus.getMessage());
+    }
 }
